2adf70f043f0cc35d4a94203de36d93509ce67e1
Damsa <dame.ivanovski@gmail.com>
2019-04-19 15:08:11 +0200

    LeapMotion UI Lab02

---
+++ Assets/PrintajNesto.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintajNesto : MonoBehaviour
+{
+    public void PrintActiveMessage()
+    {
+        Debug.Log("Active");
+
+
+    }
+    public void PrintDeactivateMessage()
+    {
+        print("Deactivate");
+    }
+
+
+
+    public void OnMouseEnter()
+    {
+       
+        Debug.Log("Button selected");
+
+    }
+
+    public void OnMouseExit()
+    {
+       
+        Debug.Log("Button deselected");
+    }
+
+}
+
